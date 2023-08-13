ec2c7fbd3ce890bdffe7ee0410d2545c227e70dd
Aleksey5757 <NelyubAleks@yandex.ru>
2023-08-13 13:48:07 +0400

    2

--- src/test/java/api/TestSortApi.java
+++ src/test/java/api/TestSortApi.java
@@ -1,25 +1,35 @@
 package api;
 
-import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
 public class TestSortApi {
     private final static String URL = "http://77.50.236.203:4879/";
     @Test
-    public void check(){
+    public void sort(){
+        Specifications.installSpecification(Specifications.requrstSpec(URL),Specifications.responseSpecOK200());
         List<UserData> users = given()
                 .when()
-                .contentType(ContentType.JSON)
-                .get(URL+"users")
+                .get("users")
                 .then().log().all()
                 .extract().body().jsonPath().getList("root", UserData.class);
-        List<Integer> id = users.stream().map(UserData::getId).collect(Collectors.toList());
-        List<Integer> sortID = id.stream().sorted().collect(Collectors.toList());
-        System.out.println(sortID);
+
+
+
+        //List<Integer> id = users.stream().map(UserData::getId).collect(Collectors.toList());
+        //List<Integer> sortID = id.stream().sorted().collect(Collectors.toList());
+        //System.out.println(sortID);
     }
+
+    @Test
+    public void check(){
+
+
+
+
+    }
+
 }
